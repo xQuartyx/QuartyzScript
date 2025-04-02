018766150ae5634a5f0a61c7cd8b0e213af84b58
Quartyz <95701206+xQuartyx@users.noreply.github.com>
2025-04-03 00:09:02 +0700

    Create Loader.lua

---
+++ Blox Fruits/Loader.lua
@@ -0,0 +1,5 @@
+if Mode == "PVP" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/Combat.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/Default.lua"))()
+end
