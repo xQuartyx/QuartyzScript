58c498272a0879399a19e90dff0e961a43cc7f4e
Quartyz <95701206+xQuartyx@users.noreply.github.com>
2025-04-04 04:06:45 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,7 +4,8 @@ local GameId = game.GameId
 
 local GameList = {
     [5682590751] = "Lootify",
-    [994732206] = "Blox%20Fruits/Loader.lua"
+    [994732206] = "Blox%20Fruits/Loader.lua",
+    [1451439645] = "King%20Legacy/Loader.lua"
 }
 
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/%s"):format(GameList[GameId])))()
