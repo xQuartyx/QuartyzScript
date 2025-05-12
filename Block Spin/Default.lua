b077883e725127e44afeb1128bbb0bf367c3f921
Quartyz <95701206+xQuartyx@users.noreply.github.com>
2025-05-12 22:04:18 +0700

    Update Default.lua

--- Block Spin/Default.lua
+++ Block Spin/Default.lua
@@ -1,3 +1,3 @@
 repeat task.wait() until game:IsLoaded()
 
-game.Players.LocalPlayer:Kick("\n\nScript Patched, Update in progress")
+game.Players.LocalPlayer:Kick("\nScript Patched, Update in progress")
