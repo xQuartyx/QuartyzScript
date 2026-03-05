3d33f3b5a18942eb83ba8e143f4ed3b341ecaee8
Quartyz <95701206+xQuartyx@users.noreply.github.com>
2026-03-06 01:20:42 +0700

    Update loader.lua

--- OneClick/loader.lua
+++ OneClick/loader.lua
@@ -1,5 +1,7 @@
 repeat task.wait() until game:IsLoaded()
 
 if getgenv().QuartyzV4Config then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Blox%20Fruits/V4.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Blox%20Fruits/v4.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Blox%20Fruits/main.lua"))()
 end
