8da9b7e883d57b7c807b705122952f6316c3cf04
Quartyz <95701206+xQuartyx@users.noreply.github.com>
2026-03-06 01:17:44 +0700

    Update and rename loader.luau to loader.lua

---
+++ OneClick/loader.lua
@@ -0,0 +1,5 @@
+repeat task.wait() until game:IsLoaded()
+
+if getgenv().QuartyzV4Config then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Blox%20Fruits/V4.lua"))()
+end
