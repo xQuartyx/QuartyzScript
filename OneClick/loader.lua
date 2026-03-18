c862255313de78ed220be243b28fe56db9e6776a
Quartyz <95701206+xQuartyx@users.noreply.github.com>
2026-03-18 21:15:50 +0700

    Update loader.lua

--- OneClick/loader.lua
+++ OneClick/loader.lua
@@ -1,7 +1,11 @@
 repeat task.wait() until game:IsLoaded()
 
-if getgenv().QuartyzV4Config then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Blox%20Fruits/v4.lua"))()
+if game.GameId == 9186719164 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Sailor%20Piece/main.lua"))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Blox%20Fruits/main.lua"))()
+    if getgenv().QuartyzV4Config then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Blox%20Fruits/v4.lua"))()
+    else
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Blox%20Fruits/main.lua"))()
+    end
 end
