927886915b845242d89decd449979addc8ce4a84
Quartyz <95701206+xQuartyx@users.noreply.github.com>
2026-04-05 00:32:55 +0700

    Update loader.lua

--- OneClick/loader.lua
+++ OneClick/loader.lua
@@ -1,6 +1,11 @@
 repeat task.wait() until game:IsLoaded()
 
 if game.GameId == 9186719164 then
+    if getgenv().TradeConfig then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Sailor%20Piece/tradeTo.lua"))()
+        return
+    end
+    
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xQuartyx/QuartyzScript/main/OneClick/Sailor%20Piece/main.lua"))()
 else
     if getgenv().QuartyzV4Config then
